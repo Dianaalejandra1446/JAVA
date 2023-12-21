d7394dead87c69446c0527624f6d7f3cb0b124fd
a <a>
2023-12-20 21:15:57 -0500

    Avance

--- src/NumeroInvertido.java
+++ src/NumeroInvertido.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class NumeroInvertido {
-    
+    public static void main(String[] args) {
+        int invertido = 0;
+        int resto;
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese un numero: ");
+        int numero = leer.nextInt();
+        while (numero > 0) {
+             resto = numero % 10;
+             invertido = ( invertido * 10) + resto;
+            numero /= 10;
+        }
+        System.out.println("Numero invertido: "+ invertido);
+    }
 }
